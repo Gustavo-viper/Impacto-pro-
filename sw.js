54aa6a53d29f901d110049b09d97650770b1fe6e
Gustavo-viper <studioailvatattoo@gmail.com>
2026-09-24 19:32:27 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C="impacto-pro-v27";
+const C="impacto-pro-v28";
 const A=[
   "./","./index.html","./style.css","./app.js","./manifest.json","./version.json",
   "./assets/impacto-pro-logo-transparent.png","./assets/impacto-pro-logo.jpeg","./assets/impacto-pro-logo-original.png","./assets/impacto-pro-marca-dagua-a4.jpg",
